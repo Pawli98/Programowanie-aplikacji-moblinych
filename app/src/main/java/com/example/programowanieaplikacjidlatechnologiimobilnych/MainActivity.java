ef3b39c710a2ac282719db4b0aae6801abdee1da
Pawli98 <pawel.gruhn@gmail.com>
2020-04-11 10:50:59 +0200

    koniec projektu

--- app/src/main/java/com/example/programowanieaplikacjidlatechnologiimobilnych/MainActivity.java
+++ app/src/main/java/com/example/programowanieaplikacjidlatechnologiimobilnych/MainActivity.java
@@ -7,15 +7,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> target;
-    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
+    MySQLite db=new MySQLite(this);
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -27,9 +32,35 @@ public class MainActivity extends AppCompatActivity {
                     };
         this.target=new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
-        this.adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
+        this.adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, db.lista(),new String[] {"_id", "gatunek"}, new int[] {android.R.id.text1,android.R.id.text2},SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
         ListView listview = (ListView) findViewById(R.id.listView );
         listview.setAdapter(this.adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int pos, long id)
+            {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+                Intent intencja = new Intent(getApplicationContext(), DodajWpis.class);
+                intencja.putExtra("element", zwierz);
+                startActivityForResult(intencja, 2);
+            }
+        });
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+
+                db.usun(name.getText().toString());
+
+                adapter.changeCursor(db.lista());
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
 
@@ -42,19 +73,28 @@ public class MainActivity extends AppCompatActivity {
     }
     public void nowyWpis(MenuItem mi)
     {
-        Intent intencja = new Intent(this,
-                DodajWpis.class);
+        Intent intencja = new Intent(this, DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
 
     @Override
-    protected void onActivityResult(
-            int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
+        if (requestCode == 1 && resultCode == RESULT_OK)
+        {
             Bundle extras = data.getExtras();
-            String nowy = (String) extras.get("wpis");
-            target.add(nowy);
+            Animal nowy = (Animal) extras.getSerializable("nowy");
+            this.db.dodaj(nowy);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
+        }
+        if (requestCode == 2)
+        {
+            Bundle extras = data.getExtras();
+            Animal nowy = (Animal)extras.getSerializable("nowy");
+            this.db.aktualizuj(nowy);
+            adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
         }
     }
